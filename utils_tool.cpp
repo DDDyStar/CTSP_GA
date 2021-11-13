2663abf9c28dfdbb25f0a2ce3f6c83edeb6fceb1
dyx <a460853000@qq.com>
2021-11-13 16:59:13 +0800

    add print_vector function

--- utils_tool.cpp
+++ utils_tool.cpp
@@ -8,3 +8,10 @@ double get_random_double(int rangeMax) {
 	return 1.0 * rangeMax * rand() / (RAND_MAX + 1);
 }
 
+void print_vector_int(vector<int> v) {
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
--- utils_tool.h
+++ utils_tool.h
@@ -2,7 +2,9 @@
 #include<cmath>
 #include<ctime>
 #include<iostream>
+#include<vector>
 using namespace std;
 
 double get_random_double(int rangeMax);
 int getRandomInt(int rangeMax);
+void print_vector_int(vector<int> v);
